b7a8e17cca920e31755ea1272670956a4f50eaa7
Flaminglouw <132774069+Flaminglouw@users.noreply.github.com>
2023-05-28 09:45:15 +0200

    1

--- script.js
+++ script.js
@@ -1,67 +1,61 @@
 const plates = [
-  "IshiharaTestPlates1.png",
-  "IshiharaTestPlates2.png",
-  "IshiharaTestPlates3.png",
-  "IshiharaTestPlates4.png",
-  "IshiharaTestPlates5.png",
-  "IshiharaTestPlates6.png",
-  "IshiharaTestPlates7.png",
-  "IshiharaTestPlates8.png",
-  "IshiharaTestPlates9.png",
-  "IshiharaTestPlates10.png",
-  "IshiharaTestPlates11.png",
-  "IshiharaTestPlates12.png",
-  "IshiharaTestPlates13.png",
-  "IshiharaTestPlates14.png",
-  "IshiharaTestPlates15.png",
-  "IshiharaTestPlates16.png",
-  "IshiharaTestPlates17.png",
-  "IshiharaTestPlates18.png",
-  "IshiharaTestPlates19.png",
-  "IshiharaTestPlates20.png",
-  "IshiharaTestPlates21.png",
-  "IshiharaTestPlates22.png",
-  "IshiharaTestPlates23.png",
-  "IshiharaTestPlates24.png",
+  { plate: "IshiharaTestPlates1.png", answer: "12" },
+  { plate: "IshiharaTestPlates2.png", answer: "8" },
+  { plate: "IshiharaTestPlates3.png", answer: "29" },
+  { plate: "IshiharaTestPlates4.png", answer: "5" },
+  { plate: "IshiharaTestPlates5.png", answer: "3" },
+  { plate: "IshiharaTestPlates6.png", answer: "15" },
+  { plate: "IshiharaTestPlates7.png", answer: "74" },
+  { plate: "IshiharaTestPlates8.png", answer: "6" },
+  { plate: "IshiharaTestPlates9.png", answer: "45" },
+  { plate: "IshiharaTestPlates10.png", answer: "5" },
+  { plate: "IshiharaTestPlates11.png", answer: "7" },
+  { plate: "IshiharaTestPlates12.png", answer: "16" },
+  { plate: "IshiharaTestPlates13.png", answer: "73" },
+  { plate: "IshiharaTestPlates14.png", answer: "0" },
+  { plate: "IshiharaTestPlates15.png", answer: "0" },
+  { plate: "IshiharaTestPlates16.png", answer: "26" },
+  { plate: "IshiharaTestPlates17.png", answer: "42" },
+  { plate: "IshiharaTestPlates18.png", answer: "2" },
+  { plate: "IshiharaTestPlates19.png", answer: "0" },
+  { plate: "IshiharaTestPlates20.png", answer: "1" },
+  { plate: "IshiharaTestPlates21.png", answer: "1" },
+  { plate: "IshiharaTestPlates22.png", answer: "1" },
+  { plate: "IshiharaTestPlates23.png", answer: "1" },
+  { plate: "IshiharaTestPlates24.png", answer: "1" },
 ];
 
-const correctAnswers = [
-  "12",
-  "8",
-  "29",
-  "5",
-  "3",
-  "15",
-  "74",
-  "6",
-  "45",
-  "5",
-  "7",
-  "16",
-  "73",
-  "0",
-  "0",
-  "26",
-  "42",
-  "2",
-  "0",
-  "1",
-  "1",
-  "1",
-  "1",
-  "1",
-];
+// Shuffle the plates array
+const shuffledPlates = shuffle(plates);
 
-let currentPlateIndex = 0;
+// Function to shuffle an array using Fisher-Yates algorithm
+function shuffle(array) {
+  let currentIndex = array.length;
+  let temporaryValue, randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 const ishiharaImage = document.getElementById("ishihara-image");
 const answerInput = document.getElementById("answer");
 const hintContainer = document.getElementById("hint");
 const nextButton = document.getElementById("next-button");
 
+let currentPlateIndex = 0;
+
 // Function to display the current plate
 function displayPlate() {
-  ishiharaImage.src = plates[currentPlateIndex];
+  const currentPlate = shuffledPlates[currentPlateIndex];
+  ishiharaImage.src = currentPlate.plate;
   answerInput.value = "";
   hintContainer.textContent = "";
   nextButton.disabled = true;
@@ -69,11 +63,11 @@ function displayPlate() {
 
 // Function to check the answer
 function checkAnswer() {
-  const answer = answerInput.value.trim();
-  const correctAnswer = correctAnswers[currentPlateIndex].toLowerCase();
+  const answer = answerInput.value.trim().toLowerCase();
+  const currentPlate = shuffledPlates[currentPlateIndex];
 
   // Check if the answer is correct
-  if (answer.toLowerCase() === correctAnswer) {
+  if (answer === currentPlate.answer.toLowerCase()) {
     // Display a success message
     hintContainer.textContent = "LEKKER MAN IZIhara";
 
@@ -91,7 +85,7 @@ function goToNextPlate() {
   currentPlateIndex++;
 
   // Check if we have reached the end of the plates array
-  if (currentPlateIndex >= plates.length) {
+  if (currentPlateIndex >= shuffledPlates.length) {
     // Reset the index to cycle back to the first plate
     currentPlateIndex = 0;
   }
